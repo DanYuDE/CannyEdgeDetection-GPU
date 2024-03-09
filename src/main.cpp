73ab70a5f24a3a7b731bfa46672d6e35e1c8f811
Jatin-Patel2331 <jatinptl2331@gmail.com>
2024-03-09 14:32:50 +0100

    main

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,7 @@
 
 using namespace std;
 using namespace cv;
+using namespace cl;
 
 bool runCpu = true;
 bool runGpu = true;
@@ -51,7 +52,6 @@ Mat applyHysteresis(Mat& strong, const Mat& weak);
 int main(int argc, char* argv[]){
 
   cout << "Beginning of the project!" << endl;
-   
   // load image
   String imgName;
   if (argc > 1)
@@ -68,10 +68,76 @@ int main(int argc, char* argv[]){
     return 0;
   }
 
-  imshow("src", src);
+  //imshow("src", src);
+  //cpuFunction(src);
 
-  cpuFunction(src);
+  Context context(CL_DEVICE_TYPE_GPU); 
+  
+  cout << "Context has " << context.getInfo<CL_CONTEXT_DEVICES>().size() << " devices" << endl;
+  Device gpu_device = context.getInfo<CL_CONTEXT_DEVICES>()[0];
+  vector<Device> devices_list;
+  devices_list.push_back(gpu_device);
+  OpenCL::printDeviceInfo(cout, gpu_device);
+  
+  
+  CommandQueue queue(context, gpu_device, CL_QUEUE_PROFILING_ENABLE);
 
+  extern unsigned char canny_edge_cl[];
+  extern unsigned int canny_edge_cl_len; 
+  Program canny_edge(context, string((const char*)canny_edge_cl, canny_edge_cl_len));
+  
+  OpenCL::buildProgram(canny_edge, devices_list);
+
+ // Declare some values
+  size_t wgSizeX =
+      16;  // Number of work items per work group in X direction
+  size_t wgSizeY = 16;
+  size_t countX =
+      wgSizeX *
+      40;  // Overall number of work items in X direction = Number of elements in X direction
+  size_t countY = wgSizeY * 30;
+  //countX *= 3; countY *= 3;
+  size_t count = countX * countY;       // Overall number of elements
+  size_t size = count * sizeof(float);  // Size of data in bytes
+  
+  vector<float> src_img(count);
+  vector<float> outputCpu_img(count);
+  vector<float> outputGpu_img(count);
+
+  // Allocate space for input and output data on the device
+  Buffer d_input(context, CL_MEM_READ_WRITE, size);
+  Buffer d_output(context, CL_MEM_READ_WRITE, size);
+
+  memset(src_img.data(), 255, size);
+  memset(outputCpu_img.data(), 255, size);
+  memset(outputGpu_img.data(), 255, size);
+  queue.enqueueWriteBuffer(d_input, true, 0, size, h_input.data());
+  queue.enqueueWriteBuffer(d_output, true, 0, size, h_outputGpu.data());
+
+  std::vector<float> inputData;
+  std::size_t inputWidth, inputHeight;
+  Core::readImagePGM("imgPath", inputData, inputWidth, inputHeight);
+  for (size_t j = 0; j < countY; j++) {
+    for (size_t i = 0; i < countX; i++) {
+      src_img[i + countX * j] =
+      inputData[(i % inputWidth) + inputWidth * (j % inputHeight)];
+    }
+  }
+
+//copy input data to device
+  Event copy1;
+  Image2D image;
+  image = cl::Image2D(context, CL_MEM_READ_ONLY,
+                          cl::ImageFormat(CL_R, CL_FLOAT), countX, countY);
+      cl::size_t<3> origin;
+      origin[0] = origin[1] = origin[2] = 0;
+      cl::size_t<3> region;
+      region[0] = countX;
+      region[1] = countY;
+      region[2] = 1;
+      queue.enqueueWriteImage(image, true, origin, region,
+                              countX * sizeof(float), 0, src_img.data(), NULL,
+                              &copy1);
   return 0;
 }
 
